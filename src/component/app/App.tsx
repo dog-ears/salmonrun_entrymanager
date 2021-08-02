e13e8b1dcd32802a636d32ee6ee15eee6e54d321
dog-ears <info@dog-ears.net>
2021-08-03 08:13:59 +0900

    ・不要ファイルの削除 ・cssリセットの導入 ・font-awesomeの導入

--- src/component/app/App.tsx
+++ src/component/app/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from 'logo.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>現在のゲスト</h2>
+      <div className="guests">
+        <ul>
+          <li>googleネーム/switchネーム（0）</li>
+          <li>googleネーム/switchネーム（0）</li>
+          <li>googleネーム/switchネーム（0）</li>
+        </ul>
+      </div>
+      <h2>次回のゲスト</h2>
+      <div className="guests">
+        <ul>
+          <li>googleネーム/switchネーム（0）</li>
+          <li>googleネーム/switchネーム（0）</li>
+          <li>googleネーム/switchネーム（0）</li>
+        </ul>
+      </div>
     </div>
   );
 }
