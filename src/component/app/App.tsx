618195803cd03d26864b48cc3a8b7888d5b7ff8a
dog-ears <info@dog-ears.net>
2021-08-04 05:40:42 +0900

    大雑把なモック制作

--- src/component/app/App.tsx
+++ src/component/app/App.tsx
@@ -4,22 +4,63 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h2>現在のゲスト</h2>
-      <div className="guests">
-        <ul>
-          <li>googleネーム/switchネーム（0）</li>
-          <li>googleネーム/switchネーム（0）</li>
-          <li>googleネーム/switchネーム（0）</li>
-        </ul>
-      </div>
-      <h2>次回のゲスト</h2>
-      <div className="guests">
-        <ul>
-          <li>googleネーム/switchネーム（0）</li>
-          <li>googleネーム/switchネーム（0）</li>
-          <li>googleネーム/switchネーム（0）</li>
-        </ul>
-      </div>
+      <section className="section-game">
+        <form action="">
+          <h2>現在のゲスト</h2>
+          <div className="guests">
+            <ul>
+              <li>googleネーム/switchネーム（0）</li>
+              <li>googleネーム/switchネーム（0）</li>
+              <li>googleネーム/switchネーム（0）</li>
+            </ul>
+          </div>
+          <h2>次回のゲスト</h2>
+          <div className="guests">
+            <ul>
+              <li>googleネーム/switchネーム（1）</li>
+              <li>googleネーム/switchネーム（0）</li>
+              <li>googleネーム/switchネーム（0）</li>
+            </ul>
+          </div>
+          <h2>次々回のゲスト</h2>
+          <div className="guests">
+            <ul>
+              <li>googleネーム/switchネーム（1）</li>
+              <li>googleネーム/switchネーム（0）</li>
+              <li>googleネーム/switchネーム（0）</li>
+            </ul>
+          </div>
+        </form>
+      </section>
+      <section className="section-guest">
+        <h2>ゲスト一覧</h2>
+        <div className="guests">
+          <table>
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>googleネーム</th>
+                <th>switchネーム</th>
+                <th>操作</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td><input type="text" value="googleネーム" /></td>
+                <td><input type="text" value="switchネーム" /></td>
+                <td><button></button></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="btn">
+          <button>追加</button>
+        </div>
+      </section>
+      <section className="section-setting">
+        <h2>設定</h2>
+      </section>
     </div>
   );
 }
