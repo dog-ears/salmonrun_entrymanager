537abc128ccda6a5a7c4c19ae384c3c021bff7ee
dog-ears <info@dog-ears.net>
2021-08-14 09:52:51 +0900

    スマホ時の表示調整

--- src/component/sectionGame/SectionGame.tsx
+++ src/component/sectionGame/SectionGame.tsx
@@ -97,7 +97,7 @@ const SectionGuest: React.FC<Props> = (props) => {
   return (
     <section className="section-game">
       <form action="">
-        <div className="topTtl"><span>サーモンラン</span> <span>参加型配信</span> <span>参加者管理ツール</span></div>
+        <div className="topTtl"><span>参加型配信</span> <span>参加者管理ツール</span></div>
         <h1>{results.length + 1}<small>戦目（ </small><input type="text" defaultValue="2" /><small>戦交代）</small></h1>
         <div className="result">{results.filter((result) => result.isVictory).length}<small>勝</small>{results.filter((result) => !result.isVictory).length}<small>敗</small>
           {results.length > 0 &&
